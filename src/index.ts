fd4c6e4ff9285a8a4aa684f2c7f5af216923c105
Nick Allevato <nicholas.allevato@gmail.com>
2016-12-30 11:15:11 -0500

    Added a test typescript file

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import * as fs from 'fs';
+
+function printContents(
+  fileName: string,
+  callback?: (contents: string) => void
+): void {
+
+  fs.readFile(fileName, (err, data) => {
+    if (err)
+      throw err;
+
+    if (callback) {
+      const contents = data.toString();
+      callback(contents);
+    }
+  })
+}
+
+if (fs.existsSync('sauce')) {
+  printContents('sauce', (contents) => {
+    console.log('Contents of the file are:', contents);
+  });
+} else {
+  fs.writeFile('sauce', 'noob', (err) => {
+    printContents('sauce', (contents) => {
+      console.log(`Wrote '${contents}' to the sauce file`);
+    });
+  })
+}
